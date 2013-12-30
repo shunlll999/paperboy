6b73fba702e14ec037d0ebb6b3c5722e22035727
shunlll999 <wachimac@wacharas-iMac.local>
2013-12-31 01:41:35 +0700

    start code

--- www/js/main.js
+++ www/js/main.js
@@ -9,18 +9,23 @@ var game = {
 };
 
 var gameStage;
+var logoGame;
 
 function startControl(){
 	console.log("start control");
-	var gameStage = new Phaser.Game(640, 320, Phaser.AUTO, 'gamewall', { preload: preload, create: create, update: update  });
+	gameStage = new Phaser.Game(640, 320, Phaser.AUTO, 'gamewall', { preload: preload, create: create, update: update  });
 }
 
 function preload(){
 	console.log("loading... ");
+	gameStage.load.image('logoGame', 'img/mainlogp.png');
 }
 
 function create(){
 	console.log("created... ");
+	logoGame = gameStage.add.sprite(gameStage.world.centerX, gameStage.world.centerY, 'logoGame');
+	logoGame.anchor.setTo(0.5, 0.5);
+
 }
 
 function update(){
